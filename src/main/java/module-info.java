d7ef763eac14fd011c5fc81d2065c2b0dc26be20
ImmortalIdiot <max.krasulya@yandex.ru>
2024-06-07 01:27:32 +0300

    feat: add sql require

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,9 @@
 module com.immortalidiot.clinicdb {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+    requires java.sql;
+
+
     opens com.immortalidiot.clinicdb to javafx.fxml;
     exports com.immortalidiot.clinicdb;
 }
