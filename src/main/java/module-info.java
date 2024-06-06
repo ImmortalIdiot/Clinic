465411038990f5c1acc9b5215b9b648bcaa95a54
ImmortalIdiot <max.krasulya@yandex.ru>
2024-06-07 02:15:10 +0300

    feat: add the necessary requires

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,15 @@ module com.immortalidiot.clinicdb {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
-
+    requires org.hibernate.orm.core;
+    requires jakarta.persistence;
+    requires jakarta.transaction;
+    requires jakarta.cdi;
+    requires org.jboss.logging;
+    requires jakarta.xml.bind;
+    requires com.fasterxml.classmate;
+    requires net.bytebuddy;
+    requires java.naming;
 
     opens com.immortalidiot.clinicdb to javafx.fxml;
     exports com.immortalidiot.clinicdb;
