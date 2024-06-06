dc7180b4cafd7e0bb70830040190bd2ba4319cb9
ImmortalIdiot <max.krasulya@yandex.ru>
2024-06-07 01:23:10 +0300

    feat: add the necessary database variables

---
+++ src/main/java/com/immortalidiot/clinicdb/JDBCRunner.java
@@ -0,0 +1,12 @@
+package com.immortalidiot.clinicdb;
+
+public class JDBCRunner {
+    private static final String PROTOCOL = "jdbc:postgresql://";
+    private static final String DRIVER = "org.postgresql.Driver";
+    private static final String URL_LOCALE_NAME = "localhost/";
+    private static final String URL_REMOTE = "10.242.65.114:5432/";
+    private static final String DATABASE_NAME = "clinic";
+
+    public static final String USER_NAME = "postgres";
+    public static final String DATABASE_PASS = "password";
+}
