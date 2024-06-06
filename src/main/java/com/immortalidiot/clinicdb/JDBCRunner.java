bc6ebbec9897e7fa3fdb533e46af319df7a7a2c9
ImmortalIdiot <max.krasulya@yandex.ru>
2024-06-07 01:32:24 +0300

    feat: add the database checker

--- src/main/java/com/immortalidiot/clinicdb/JDBCRunner.java
+++ src/main/java/com/immortalidiot/clinicdb/JDBCRunner.java
@@ -1,11 +1,17 @@
 package com.immortalidiot.clinicdb;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class JDBCRunner {
     private static final String PROTOCOL = "jdbc:postgresql://";
     private static final String DRIVER = "org.postgresql.Driver";
     private static final String URL_LOCALE_NAME = "localhost/";
     private static final String URL_REMOTE = "10.242.65.114:5432/";
     private static final String DATABASE_NAME = "clinic";
+
+    private static final String DATABASE_URL = PROTOCOL + URL_LOCALE_NAME + DATABASE_NAME;
 
     public static final String USER_NAME = "postgres";
     public static final String DATABASE_PASS = "password";
@@ -15,6 +21,16 @@ public class JDBCRunner {
             Class.forName(DRIVER);
         } catch (ClassNotFoundException e) {
             System.out.println("No JDBC driver! Connect the JDBC driver to the project according to the instructions.");
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void checkDatabase() {
+        try {
+            DriverManager.getConnection(DATABASE_URL, USER_NAME, DATABASE_PASS);
+            System.out.println("Success");
+        } catch (SQLException e) {
+            System.out.println("No database! Check the database name, path to the database, or deploy a local backup copy according to the instructions");
             throw new RuntimeException(e);
         }
     }
