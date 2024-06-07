aba5b9bfddd5d7bb7f814a8c624839c81d047450
ImmortalIdiot <max.krasulya@yandex.ru>
2024-06-08 01:44:26 +0300

    feat: implement sending requests

--- src/main/java/com/immortalidiot/clinicdb/controller/MondayWorkerController.java
+++ src/main/java/com/immortalidiot/clinicdb/controller/MondayWorkerController.java
@@ -1,7 +1,10 @@
 package com.immortalidiot.clinicdb.controller;
 
 import com.immortalidiot.clinicdb.HelloApplication;
+import com.immortalidiot.clinicdb.JDBCRunner;
 import com.immortalidiot.clinicdb.model.DataField;
+import com.immortalidiot.clinicdb.service.DatabaseService;
+import com.immortalidiot.clinicdb.writer.TableWriter;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -16,10 +19,11 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.Objects;
-import java.util.ResourceBundle;
+import java.util.*;
 
 public class MondayWorkerController {
+
+    private final DatabaseService databaseService = new DatabaseService(JDBCRunner.SESSION_FACTORY);
 
     @FXML
     private ResourceBundle resources;
@@ -57,7 +61,30 @@ public class MondayWorkerController {
 
     @FXML
     protected void search() {
-        //TODO: add the request
+        String text = field.getText();
+
+        if (text.isBlank()) {
+            List<DataField> data = databaseService.getAllMondayWorkers();
+            error.setText("");
+            TableWriter.write(tableView, data);
+        } else {
+            String specialization = text.substring(0, 1).toUpperCase() + text.substring(1).toLowerCase();
+            List<String> specializations = new ArrayList<>(Arrays.asList(
+                    "Терапевт",
+                    "Офтальмолог",
+                    "Гастро-интеролог",
+                    "Кардиолог",
+                    "Акушер-гинеколог"
+            ));
+
+            if (specializations.contains(specialization)) {
+                List<DataField> data = databaseService.getMondayWorkers(specialization);
+                error.setText("");
+                TableWriter.write(tableView, data);
+            } else {
+                error.setText("Неизвестная специальность");
+            }
+        }
     }
 
     @FXML
