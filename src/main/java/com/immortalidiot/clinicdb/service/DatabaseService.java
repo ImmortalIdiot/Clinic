0dd763d107733e82d359ba072d79f78c0049b8af
ImmortalIdiot <max.krasulya@yandex.ru>
2024-06-07 20:56:36 +0300

    feat: add monday workers requests

--- src/main/java/com/immortalidiot/clinicdb/service/DatabaseService.java
+++ src/main/java/com/immortalidiot/clinicdb/service/DatabaseService.java
@@ -3,8 +3,8 @@ package com.immortalidiot.clinicdb.service;
 import com.immortalidiot.clinicdb.collector.PairCollector;
 import com.immortalidiot.clinicdb.model.DataField;
 import org.hibernate.Session;
-import org.hibernate.query.Query;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.hibernate.transform.AliasToEntityMapResultTransformer;
 
 import java.util.List;
@@ -48,10 +48,39 @@ public class DatabaseService {
     public List<DataField> getPatientsByGender(String gender) {
         Session session = sessionFactory.openSession();
         Query query = session.createNativeQuery("SELECT name AS \"Имя\", " +
-                            "surname AS \"Фамилия\"," +
-                            "patronymic AS \"Отчество\"" +
-                            " FROM patients WHERE gender = :gender")
-                    .setParameter("gender", gender);
+                        "surname AS \"Фамилия\"," +
+                        "patronymic AS \"Отчество\"" +
+                        " FROM patients WHERE gender = :gender")
+                .setParameter("gender", gender);
+        return mapToDataField(query);
+    }
+
+    public List<DataField> getAllMondayWorkers() {
+        Session session = sessionFactory.openSession();
+        Query query = session.createNativeQuery("SELECT " +
+                "d.doctor_id AS \"Табельный номер\", " +
+                "d.surname AS \"Фамилия\", " +
+                "d.specialization AS \"Специальность\", " +
+                "s.time AS \"Время приёма\", " +
+                "FROM doctors d\n" +
+                "JOIN schedule s ON d.doctor_id = s.doctor_id\n" +
+                "WHERE s.day_of_week = 'MON';");
+        return mapToDataField(query);
+    }
+
+    public List<DataField> getMondayWorkers(String specialization) {
+        Session session = sessionFactory.openSession();
+        Query query = session.createNativeQuery("SELECT name AS \"Имя\", " +
+                        "surname AS \"Фамилия\", " +
+                        "patronymic AS \"Отчество\", " +
+                        "specialization AS \"Специальность\", " +
+                        "experience AS \"Стаж\" " +
+                        "FROM patients " +
+                        "WHERE specialization = :specialization\n" +
+                        "\tAND doctor_id IN \n" +
+                        "\t(SELECT doctor_id FROM public.schedule\n" +
+                        "\t WHERE day_of_week = 'MON');\n")
+                .setParameter("specialization", specialization);
         return mapToDataField(query);
     }
 }
