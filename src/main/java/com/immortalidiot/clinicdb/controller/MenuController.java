b19fad4197730819d31a8944dc7ee93507e2dc54
ImmortalIdiot <max.krasulya@yandex.ru>
2024-06-07 05:35:09 +0300

    feat: add main menu class

---
+++ src/main/java/com/immortalidiot/clinicdb/controller/MenuController.java
@@ -0,0 +1,35 @@
+package com.immortalidiot.clinicdb.controller;
+
+import com.immortalidiot.clinicdb.HelloApplication;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class MenuController {
+
+    @FXML
+    private Button one_table;
+
+    @FXML
+    void initialize() {
+        assert one_table != null : "fx:id=\"one_table\" was not injected: check your FXML file 'Untitled'.";
+    }
+
+    @FXML
+    protected void moveToPatientSearcher(ActionEvent event) throws IOException {
+        Parent firstTableView = FXMLLoader.load(Objects.requireNonNull(HelloApplication.class.getResource("patient-searcher.fxml")));
+        Scene firstTableViewScene = new Scene(firstTableView, 1024, 720);
+
+        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+
+        window.setScene(firstTableViewScene);
+    }
+}
