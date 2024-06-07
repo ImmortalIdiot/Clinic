91e47e4a37f8c7f6c2da8c55ddcab91642d18a39
ImmortalIdiot <max.krasulya@yandex.ru>
2024-06-07 20:21:21 +0300

    feat: connect menu with an monday worker controller

--- src/main/java/com/immortalidiot/clinicdb/controller/MenuController.java
+++ src/main/java/com/immortalidiot/clinicdb/controller/MenuController.java
@@ -19,13 +19,27 @@ public class MenuController {
     private Button one_table;
 
     @FXML
+    private Button monday_therapist;
+
+    @FXML
     void initialize() {
         assert one_table != null : "fx:id=\"one_table\" was not injected: check your FXML file 'Untitled'.";
+        assert monday_therapist != null : "fx:id=\"monday_therapist\" was not injected: check your FXML file 'Untitled'.";
     }
 
     @FXML
     protected void moveToPatientSearcher(ActionEvent event) throws IOException {
         Parent firstTableView = FXMLLoader.load(Objects.requireNonNull(HelloApplication.class.getResource("patient-searcher.fxml")));
+        Scene firstTableViewScene = new Scene(firstTableView, 1024, 720);
+
+        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+
+        window.setScene(firstTableViewScene);
+    }
+
+    @FXML
+    protected void moveToMondayWorker(ActionEvent event) throws IOException {
+        Parent firstTableView = FXMLLoader.load(Objects.requireNonNull(HelloApplication.class.getResource("mon-worker.fxml")));
         Scene firstTableViewScene = new Scene(firstTableView, 1024, 720);
 
         Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
