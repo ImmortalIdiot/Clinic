519b573d249119a49ef39efcc9a72be7bcb6c62b
ImmortalIdiot <max.krasulya@yandex.ru>
2024-06-08 02:47:00 +0300

    feat: overload getResonse with an boolean type param

--- src/main/java/com/immortalidiot/clinicdb/service/DatabaseService.java
+++ src/main/java/com/immortalidiot/clinicdb/service/DatabaseService.java
@@ -119,4 +119,10 @@ public class DatabaseService {
         Query query = session.createNativeQuery(request).setParameter("param", param);
         return mapToDataField(query);
     }
+
+    private List<DataField> getResponse(String request, Boolean param) {
+        Session session = sessionFactory.openSession();
+        Query query = session.createNativeQuery(request).setParameter("param", param);
+        return mapToDataField(query);
+    }
 }
