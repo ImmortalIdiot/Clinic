a295b7c581f8ffe3054123730ca8ad05046d9aeb
ImmortalIdiot <max.krasulya@yandex.ru>
2024-06-08 06:36:43 +0300

    feat: implement sending request with validation params

--- src/main/java/com/immortalidiot/clinicdb/controller/AddPatientController.java
+++ src/main/java/com/immortalidiot/clinicdb/controller/AddPatientController.java
@@ -1,7 +1,12 @@
 package com.immortalidiot.clinicdb.controller;
 
 import com.immortalidiot.clinicdb.HelloApplication;
+import com.immortalidiot.clinicdb.JDBCRunner;
+import com.immortalidiot.clinicdb.entity.MedicalCard;
+import com.immortalidiot.clinicdb.entity.Patient;
 import com.immortalidiot.clinicdb.model.DataField;
+import com.immortalidiot.clinicdb.service.DatabaseService;
+import com.immortalidiot.clinicdb.writer.TableWriter;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -13,13 +18,17 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import org.hibernate.SessionFactory;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class AddPatientController {
+
+    private final DatabaseService databaseService = new DatabaseService(JDBCRunner.SESSION_FACTORY);
 
     @FXML
     private ResourceBundle resources;
@@ -85,8 +94,102 @@ public class AddPatientController {
     }
 
     @FXML
-    void search(ActionEvent event) {
-        //TODO: implement sending request
+    void search() {
+        String surname = surnameTextField.getText();
+        String name = nameTextField.getText();
+        String patronymic = patronymicTextField.getText();
+        String age = ageTextField.getText();
+        String gender = genderTextField.getText();
+        String phoneNumber = phoneNumberTextField.getText();
+
+        try {
+            validateInputFields(surname, name, patronymic, age, gender);
+            int parsedAge = validateAge(age);
+            String parsedGender = validateGender(gender);
+
+            if (phoneNumber.isBlank()) {
+                phoneNumber = "-";
+            } else if (
+                    phoneNumber.equalsIgnoreCase("нет") ||
+                    phoneNumber.equalsIgnoreCase("отсутствует")) {
+                validatePhoneNumber(phoneNumber);
+            }
+
+            addPatient(surname, name, patronymic, parsedAge, parsedGender, phoneNumber);
+            List<DataField> data = databaseService.getPatients();
+            error.setText("");
+            TableWriter.write(addPatientTableView, data);
+
+        } catch (IllegalArgumentException e) {
+            error.setText(e.getMessage());
+        }
+    }
+
+    private void validateInputFields(String surname, String name, String patronymic, String age, String gender) {
+        if (surname.isBlank()) throw new IllegalArgumentException("Поле с фамилией не должно быть пустым!");
+        if (name.isBlank()) throw new IllegalArgumentException("Поле с именем не должно быть пустым!");
+        if (patronymic.isBlank()) throw new IllegalArgumentException("Поле с отчеством не должно быть пустым!");
+        if (age.isBlank()) throw new IllegalArgumentException("Поле с возрастом не должно быть пустым!");
+        if (gender.isBlank()) throw new IllegalArgumentException("Поле с полом не должно быть пустым!");
+    }
+
+    private String validateGender(String gender) {
+        switch (gender.toLowerCase()) {
+            case "м", "мужской", "m", "male" -> gender = "М";
+            case "ж", "женский", "f", "female" -> gender = "Ж";
+            default -> gender = "Unknown";
+        }
+        if (gender.equals("Unknown")) throw new IllegalArgumentException("Неверный ввод пола");
+        return gender;
+    }
+
+    private int validateAge(String age) {
+        try {
+            int parsedAge = Integer.parseInt(age);
+            if (parsedAge <= 0) {
+                throw new IllegalArgumentException("Возраст должен быть натуральным числом!");
+            }
+            return parsedAge;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Возраст должен быть числом!");
+        }
+    }
+
+    private static boolean isCorrectPhoneNumber(String phoneNumber) {
+        return phoneNumber.charAt(0) == '9' && phoneNumber.length() == 10;
+    }
+
+    private void validatePhoneNumber(String phoneNumber) {
+        if (!isCorrectPhoneNumber(phoneNumber)) {
+            throw new IllegalArgumentException("Неверный формат ввода номера телефона");
+        }
+    }
+
+    public void addPatient(
+            String surname,
+            String name,
+            String patronymic,
+            Integer age,
+            String gender,
+            String phoneNumber
+    ) {
+        SessionFactory sessionFactory = JDBCRunner.SESSION_FACTORY;
+
+        Patient newPatient = new Patient();
+        newPatient.surname = surname;
+        newPatient.name = name;
+        newPatient.patronymic = patronymic;
+        newPatient.age = age;
+        newPatient.gender = gender;
+        newPatient.phoneNumber = phoneNumber;
+
+        sessionFactory.inTransaction(session -> session.persist(newPatient));
+
+        MedicalCard medicalCard = new MedicalCard();
+        medicalCard.patient = newPatient;
+        medicalCard.hasDigitalCopy = false;
+
+        sessionFactory.inTransaction(session -> session.persist(medicalCard));
     }
 
     @FXML
