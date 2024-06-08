3fcdd01446de385ffb8018753062d365b1d81bc0
ImmortalIdiot <max.krasulya@yandex.ru>
2024-06-08 09:30:10 +0300

    chore: reformat code style

--- src/main/java/com/immortalidiot/clinicdb/ClinicDB.java
+++ src/main/java/com/immortalidiot/clinicdb/ClinicDB.java
@@ -11,7 +11,9 @@ public class ClinicDB extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         JDBCRunner.initDatabase();
+
         FXMLLoader fxmlLoader = new FXMLLoader(ClinicDB.class.getResource("menu.fxml"));
+
         Scene scene = new Scene(fxmlLoader.load(), 1024, 720);
         stage.setTitle("Clinic");
         stage.setScene(scene);
